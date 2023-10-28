a010648d57f08c2a1c57bad120ad20dbec9ca47f
shohibb <akmalshahibmaulana@gmail.com>
2023-10-28 22:40:15 +0700

    tugas 2

--- praktikum3/tugas2/src/components/navbar.jsx
+++ praktikum3/tugas2/src/components/navbar.jsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <div>
+      <h2>Navbar</h2>
       <nav>
         <ul>
           <li>
-            <Link to="/Home">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/About">About</Link>
