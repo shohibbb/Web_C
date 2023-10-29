c1378ea17824c3c6cc1452fc830ff45c4835a268
shohibb <akmalshahibmaulana@gmail.com>
2023-10-29 11:57:48 +0700

    mulai tugas demo

---
+++ modul3/ilab/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <h1>hello </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ modul3/ilab/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+reportWebVitals();
