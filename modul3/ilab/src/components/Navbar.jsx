2ee25481dd721546a886627a3071e21d77469324
shohibb <akmalshahibmaulana@gmail.com>
2023-10-29 14:32:01 +0700

    mulai tugas demo

--- modul3/ilab/src/components/Navbar.jsx
+++ modul3/ilab/src/components/Navbar.jsx
@@ -5,19 +5,28 @@ import "./Navbar.css";
 export const Navbar = () => {
   return (
     <div className="menu">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/About">About</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="left">
+        <img src="images/logo-ilab.png" alt="logo" srcset="" />
+      </div>
+      <div className="center">
+        <nav>
+          <ul className="horizontal-list">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="right">
+        <button className="action-button-signup">Sign Up</button>
+        <button className="action-button-login">Log In</button>
+      </div>
     </div>
   );
 };
